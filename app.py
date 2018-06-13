26b5ed7a0e5a3c42f12655e596fe06839e8d0a4f
Dimitri Moreira <taikamya@outlook.com>
2018-06-12 22:43:42 -0300

    1.1
    Signed-off-by: Dimitri Moreira <taikamya@outlook.com>

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-# Ver_0.0.1_3
+# Ver_0.0.1_2
+# import modules.database
+# import modules.security
+# import modules.user
+# import modules.ui_manager
 
 
 def main():
--- modules/security.py
+++ modules/security.py
@@ -1,5 +1,4 @@
 from werkzeug.security import safe_str_cmp
-
 from modules.user import User
 
 
